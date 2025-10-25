1729b6bf6c09221ffb4ef711fe0846cd605e5e31
andyfries <github@andyfries.com>
2025-10-25 15:21:31 -0600

    added TOC

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,44 @@
 
     checkbox.checked = false;
   }, false);
+
+  // Generate table of contents
+  function generateTOC() {
+    var tocList = document.getElementById('toc-list');
+    if (!tocList) return;
+
+    var post = document.querySelector('.post');
+    if (!post) return;
+
+    var headings = post.querySelectorAll('h2, h3');
+    if (headings.length === 0) {
+      var tocSidebar = document.querySelector('.toc-sidebar');
+      if (tocSidebar) tocSidebar.style.display = 'none';
+      return;
+    }
+
+    headings.forEach(function(heading, index) {
+      // Add ID to heading if it doesn't have one
+      if (!heading.id) {
+        heading.id = 'heading-' + index;
+      }
+
+      var li = document.createElement('li');
+      li.className = heading.tagName.toLowerCase();
+
+      var a = document.createElement('a');
+      a.href = '#' + heading.id;
+      a.textContent = heading.textContent;
+
+      li.appendChild(a);
+      tocList.appendChild(li);
+    });
+  }
+
+  // Run after DOM is loaded
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', generateTOC);
+  } else {
+    generateTOC();
+  }
 })(document);
